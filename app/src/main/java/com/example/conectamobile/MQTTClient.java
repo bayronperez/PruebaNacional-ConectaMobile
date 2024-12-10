7444fec149fe16dfc0d6ca5607b121ade2ed45a1
hidal <hidalgobayron05@gmail.com>
2024-12-10 04:37:18 -0300

    Commit inicial del proyecto

--- app/src/main/java/com/example/conectamobile/MQTTClient.java
+++ app/src/main/java/com/example/conectamobile/MQTTClient.java
@@ -41,42 +41,59 @@ public class MQTTClient {
             }
         });
     }
-
+    // Método para conectar al servidor MQTT
     public void connect(String username, String password, IMqttActionListener callback) {
         try {
+            // Crear opciones de conexión
             MqttConnectOptions options = new MqttConnectOptions();
             options.setCleanSession(true);
             options.setUserName(username);
             options.setPassword(password.toCharArray());
 
+            // Intentar conectar al servidor MQTT usando las opciones configuradas
             client.connect(options, null, callback);
         } catch (MqttException e) {
+
+            // Registrar el error si la conexión falla
             Log.e(TAG, "Error al intentar conectar: " + e.getMessage());
         }
     }
 
+    // Método para suscribirse a un topic específico
     public void subscribe(String topic, int qos, IMqttActionListener callback) {
         try {
             client.subscribe(topic, qos, null, callback);
             Log.d(TAG, "Suscrito al topic: " + topic);
         } catch (MqttException e) {
+
+            // Registrar el error si la suscripción falla
             Log.e(TAG, "Error al intentar suscribirse al topic: " + topic + ". " + e.getMessage());
         }
     }
 
+    // Método para publicar un mensaje en un topic específico
     public void publish(String topic, String payload, int qos) {
         try {
+
+            // Crear un mensaje MQTT con la carga útil especificada
             MqttMessage message = new MqttMessage(payload.getBytes());
             message.setQos(qos);
+
+            // Publicar el mensaje en el topic especificado
             client.publish(topic, message);
             Log.d(TAG, "Mensaje publicado en el topic: " + topic + ", contenido: " + payload);
         } catch (MqttException e) {
+
+            // Registrar el error si la publicación falla
             Log.e(TAG, "Error al intentar publicar en el topic: " + topic + ". " + e.getMessage());
         }
     }
 
+    // Método para desconectar el cliente del servidor MQTT
     public void disconnect() {
         try {
+
+            // Verificar si el cliente está conectado antes de intentar desconectarlo
             if (client.isConnected()) {
                 client.disconnect();
                 Log.d(TAG, "Cliente desconectado exitosamente");
@@ -86,6 +103,7 @@ public class MQTTClient {
         }
     }
 
+    // Método para verificar si el cliente está conectado al servidor MQTT
     public boolean isConnected() {
         return client.isConnected();
     }
